473515d4702a108dfef144dd5b1a378f3d00c1e2
lixiaohu123147 <7595644+lixiaohu123147@user.noreply.gitee.com>
2020-06-03 19:09:38 +0800

    页面布局

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,6 +1,8 @@
 <template>
-  <div>
-    Money.vue
+  <div class="nav-wrapper">
+    <div class="content">
+      Money.vue
+    </div>
     <Nav />
   </div>
 </template>
@@ -12,4 +14,14 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.nav-wrapper {
+  border: 1px solid green;
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+}
+.content{
+  flex-grow: 1; //尽量把所有的给它
+  overflow: auto;
+}
 </style>
