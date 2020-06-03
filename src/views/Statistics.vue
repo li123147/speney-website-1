5bf8a9b76c706d30012d501eb1338c0872d46e9b
lixiaohu123147 <7595644+lixiaohu123147@user.noreply.gitee.com>
2020-06-03 16:39:24 +0800

    “创建三个页面”

---
+++ src/views/Statistics.vue
@@ -0,0 +1,13 @@
+import Statistics from '@/views/Statistics.vue';
+<template>
+  <div>Statistics.vue</div>
+</template>
+
+<script lang="ts">
+export default {
+name:'Statistics'
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
