7c9c10435054b398df9ae6ee31e014e23a0612da
lixiaohu123147 <7595644+lixiaohu123147@user.noreply.gitee.com>
2020-06-06 18:20:09 +0800

    收集四个组件的数据到record

--- src/components/Money/NumberPad.vue
+++ src/components/Money/NumberPad.vue
@@ -22,11 +22,10 @@
 
 <script lang="ts">
 import Vue from "vue";
-import { Component } from "vue-property-decorator";
+import { Component, Prop } from "vue-property-decorator";
 @Component
 export default class NumberPad extends Vue {
   output: string = "0";
-
   inputContent(event: MouseEvent) {
     const button = event.target as HTMLButtonElement;
     const input = button.textContent!;
@@ -56,7 +55,8 @@ export default class NumberPad extends Vue {
   clear() {
     this.output = "0";
   }
-  ok() { 
+  ok() {
+    this.$emit("update:value", this.output);
   }
 }
 </script>
--- src/components/Money/Types.vue
+++ src/components/Money/Types.vue
@@ -11,16 +11,16 @@
 
 <script lang="ts">
 import Vue from "vue";
-import { Component } from "vue-property-decorator";
+import { Component, Prop, Watch} from "vue-property-decorator";
 
 @Component
 export default class Types extends Vue {
-  type = "-"; //'-'表示支出，‘+’表示收入
+  @Prop() readonly value!:string;
   selectType(type: string) {
     if (type !== "-" && type !== "+") {
       throw new Error("type is unknown");
     }
-    this.type = type;
+    this.$emit('update:value', type);
   }
 }
 </script>
