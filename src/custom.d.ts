8cf1ae62e9771c354fd16004d0cc667502e0a31f
lixiaohu123147 <7595644+lixiaohu123147@user.noreply.gitee.com>
2020-06-07 11:53:45 +0800

    封装一个model

---
+++ src/custom.d.ts
@@ -0,0 +1,8 @@
+type RecordItem = {
+  tags: string[];
+  notes: string;
+  type: string;
+  amount: number;
+  createdAt?: Date; //加？表示可以不存在
+};
+
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,40 +1,28 @@
 <template>
   <Layout class-prefix="layout">
-    {{recordList}}
     <NumberPad :value.sync="record.amount" @submit="saveRecord" />
-    <types :value.sync="record.type" />
+    <Types :value.sync="record.type" />
     <Notes @update:value="onUpdateNotes" />
     <Tags :data-source.sync="tags" @update:value="onUpdateTags" />
   </Layout>
 </template>
 
 <script lang="ts">
+import Vue from "vue";
 import NumberPad from "@/components/Money/NumberPad.vue";
 import Types from "@/components/Money/Types.vue";
 import Notes from "@/components/Money/Notes.vue";
 import Tags from "@/components/Money/Tags.vue";
-import Vue from "vue";
 import { Component, Watch } from "vue-property-decorator";
-
-const recordList: Record[] = JSON.parse(
-  window.localStorage.getItem("recordList") || "[]"
-);
-type Record = {
-  tags: string[];
-  notes: string;
-  type: string;
-  amount: number;
-  createdAt?: Date; //加？表示可以不存在
-};
-
+import model from "@/model";
+const recordList = model.fetch();
 @Component({
   components: { Tags, Notes, Types, NumberPad }
 })
 export default class Money extends Vue {
-  tags = ["衣", "食", "住", "行"];
-
-  recordList: Record[] = recordList;
-  record: Record = {
+  tags = ["衣", "食", "住", "行", "彩票"];
+  recordList: RecordItem[] = recordList;
+  record: RecordItem = {
     tags: [],
     notes: "",
     type: "-",
@@ -47,13 +35,13 @@ export default class Money extends Vue {
     this.record.notes = value;
   }
   saveRecord() {
-    const record2: Record = JSON.parse(JSON.stringify(this.record));
+    const record2: RecordItem = model.clone(this.record);
     record2.createdAt = new Date();
     this.recordList.push(record2);
   }
   @Watch("recordList")
   onRecordListChange() {
-    window.localStorage.setItem("recordList", JSON.stringify(this.recordList));
+    model.save(this.recordList);
   }
 }
 </script>
