8ab3c841fb8e0f59ececc04330df7b728b40fe1a
lixiaohu123147 <7595644+lixiaohu123147@user.noreply.gitee.com>
2020-06-08 00:16:54 +0800

    添加 EditLabel.vue 的样式，抽离了两个组件 Button 和 FormItem

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -1,16 +1,18 @@
 <template>
   <Layout>
     <div class="tags">
-      <router-link class="tag" 
-      v-for="tag in tags" :key="tag.id" 
-      :to="`/Labels/edit/${tag.id}`">
+      <router-link class="tag"
+                   v-for="tag in tags" :key="tag.id"
+                   :to="`/labels/edit/${tag.id}`">
         <span>{{tag.name}}</span>
         <Icon name="right"/>
       </router-link>
-      <!--router-link 用于路由跳转页面 -->
     </div>
     <div class="createTag-wrapper">
-      <button class="createTag" @click="createTag">新建标签</button>
+      <Button class="createTag"
+              @click="createTag">
+        新建标签
+      </Button>
     </div>
   </Layout>
 </template>
@@ -19,8 +21,11 @@
   import Vue from 'vue';
   import {Component} from 'vue-property-decorator';
   import tagListModel from '@/models/tagListModel';
+  import Button from '@/components/Button.vue';
   tagListModel.fetch();
-  @Component
+  @Component({
+    components: {Button}
+  })
   export default class Labels extends Vue {
     tags = tagListModel.data;
     createTag() {
@@ -38,35 +43,35 @@
 </script>
 
 <style lang="scss" scoped>
-.tags {
-  background: white;
-  font-size: 16px;
-  padding-left: 16px;
-  > .tag {
-    min-height: 44px;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    border-bottom: 1px solid #e6e6e6;
-    svg {
-      width: 18px;
-      height: 18px;
-      color: #666;
-      margin-right: 16px;
+  .tags {
+    background: white;
+    font-size: 16px;
+    padding-left: 16px;
+    > .tag {
+      min-height: 44px;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      border-bottom: 1px solid #e6e6e6;
+      svg {
+        width: 18px;
+        height: 18px;
+        color: #666;
+        margin-right: 16px;
+      }
     }
   }
-}
-.createTag {
-  background: #767676;
-  color: white;
-  border-radius: 4px;
-  border: none;
-  height: 40px;
-  padding: 0 16px;
-  &-wrapper {
-    text-align: center; //内联元素居中在父元素用
-    padding: 16px;
-    margin-top: 28px;
+  .createTag {
+    background: #767676;
+    color: white;
+    border-radius: 4px;
+    border: none;
+    height: 40px;
+    padding: 0 16px;
+    &-wrapper {
+      text-align: center;
+      padding: 16px;
+      margin-top: 44-16px;
+    }
   }
-}
 </style>
