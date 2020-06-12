51cbf2f13da4969e89f6649a7ff8dbc5ae9e9601
lixiaohu123147 <7595644+lixiaohu123147@user.noreply.gitee.com>
2020-06-12 21:12:04 +0800

    初步搞定项目

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,69 +1,70 @@
 <template>
   <Layout>
-    <Tabs class-prefix="type" :data-source="recordTypeList" :value.sync="type"/>
-    <ol>
+    <Tabs class-prefix="type" :data-source="recordTypeList" :value.sync="type" />
+    <ol v-if="groupedList.length>0">
       <li v-for="(group, index) in groupedList" :key="index">
-        <h3 class="title">{{beautify(group.title)}} <span>￥{{group.total}}</span></h3>
+        <h3 class="title">
+          {{beautify(group.title)}}
+          <span>￥{{group.total}}</span>
+        </h3>
         <ol>
-          <li v-for="item in group.items" :key="item.id"
-              class="record"
-          >
+          <li v-for="item in group.items" :key="item.id" class="record">
             <span>{{tagString(item.tags)}}</span>
             <span class="notes">{{item.notes}}</span>
-            <span>￥{{item.amount}} </span>
+            <span>￥{{item.amount}}</span>
           </li>
         </ol>
       </li>
     </ol>
+    <div v-else class="noResult">还木有相关记录</div>
   </Layout>
 </template>
 
 <script lang="ts">
- import Vue from 'vue';
-  import {Component} from 'vue-property-decorator';
-  import Tabs from '@/components/Tabs.vue';
-  import recordTypeList from '@/constants/recordTypeList';
-  import dayjs from 'dayjs';
-  import clone from '@/lib/clone';
+import Vue from "vue";
+import { Component } from "vue-property-decorator";
+import Tabs from "@/components/Tabs.vue";
+import recordTypeList from "@/constants/recordTypeList";
+import dayjs from "dayjs";
+import clone from "@/lib/clone";
 
- @Component({
-    components: {Tabs},
-  })
-  export default class Statistics extends Vue {
-      tagString(tags: Tag[]) {
-      return tags.length === 0 ? '无' : tags.join(',');
+@Component({
+  components: { Tabs }
+})
+export default class Statistics extends Vue {
+  tagString(tags: Tag[]) {
+    return tags.length === 0 ? "无" : tags.map(t => t.name).join("，");
+  }
+  beautify(string: string) {
+    const day = dayjs(string);
+    const now = dayjs();
+    if (day.isSame(now, "day")) {
+      return "今天";
+    } else if (day.isSame(now.subtract(1, "day"), "day")) {
+      console.log("hi");
+      return "昨天";
+    } else if (day.isSame(now.subtract(2, "day"), "day")) {
+      return "前天";
+    } else if (day.isSame(now, "year")) {
+      return day.format("M月D日");
+    } else {
+      return day.format("YYYY年M月D日");
     }
-    beautify(string: string) {
-      const day = dayjs(string);
-      const now = dayjs();
-      if (day.isSame(now, 'day')) {
-        return '今天';
-      } else if (day.isSame(now.subtract(1, 'day'), 'day')) {
-        console.log('hi');
-        return '昨天';
-      } else if (day.isSame(now.subtract(2, 'day'), 'day')) {
-        return '前天';
-      } else if (day.isSame(now, 'year')) {
-        return day.format('M月D日');
-      } else {
-        return day.format('YYYY年M月D日');
-      }
-    }
+  }
 
   get recordList() {
     return (this.$store.state as RootState).recordList;
   }
   get groupedList() {
     const { recordList } = this;
-    if (recordList.length === 0) {
-      return [];
-    }
-
     const newList = clone(recordList)
       .filter(r => r.type === this.type)
       .sort(
         (a, b) => dayjs(b.createdAt).valueOf() - dayjs(a.createdAt).valueOf()
       );
+    if (newList.length === 0) {
+      return [];
+    }
     type Result = { title: string; total?: number; items: RecordItem[] }[];
     const result: Result = [
       {
@@ -101,37 +102,41 @@
 </script>
 
 <style scoped lang="scss">
-  ::v-deep {
-    .type-tabs-item {
-      background: #C4C4C4;
-      &.selected {
-        background: white;
-        &::after {
-          display: none;
-        }
+.noResult{
+ padding: 16px;;
+ text-align: center;
+}
+::v-deep {
+  .type-tabs-item {
+    background: white;
+    &.selected {
+      background: #c4c4c4;
+      &::after {
+        display: none;
       }
     }
-    .interval-tabs-item {
-      height: 48px;
-    }
   }
-  %item {
-    padding: 8px 16px;
-    line-height: 24px;
-    display: flex;
-    justify-content: space-between;
-    align-content: center;
+  .interval-tabs-item {
+    height: 48px;
   }
-  .title {
-    @extend %item;
-  }
-  .record {
-    background: white;
-    @extend %item;
-  }
-  .notes {
-    margin-right: auto;
-    margin-left: 16px;
-    color: #999;
-  }
+}
+%item {
+  padding: 8px 16px;
+  line-height: 24px;
+  display: flex;
+  justify-content: space-between;
+  align-content: center;
+}
+.title {
+  @extend %item;
+}
+.record {
+  background: white;
+  @extend %item;
+}
+.notes {
+  margin-right: auto;
+  margin-left: 16px;
+  color: #999;
+}
 </style>
