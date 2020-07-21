f389392116a95fd11a173dc2904d7e2912a1890c
lixiaohu123147 <7595644+lixiaohu123147@user.noreply.gitee.com>
2020-07-21 16:40:43 +0800

    修复bug  完善echarts样式

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,7 +1,9 @@
 <template>
   <Layout>
     <Tabs class-prefix="type" :data-source="recordTypeList" :value.sync="type" />
-    <Chart :options="x"/>
+    <div class="chart-wrapper" ref="chartWrapper">
+      <Chart class="chart" :options="x" />
+    </div>
     <ol v-if="groupedList.length>0">
       <li v-for="(group, index) in groupedList" :key="index">
         <h3 class="title">
@@ -17,10 +19,9 @@
         </ol>
       </li>
     </ol>
-    <div v-else class="noResult">还木有相关记录</div>
+    <div v-else class="noResult">目前没有相关记录</div>
   </Layout>
 </template>
-
 <script lang="ts">
 import Vue from "vue";
 import { Component } from "vue-property-decorator";
@@ -28,15 +29,17 @@ import Tabs from "@/components/Tabs.vue";
 import recordTypeList from "@/constants/recordTypeList";
 import dayjs from "dayjs";
 import clone from "@/lib/clone";
-import Chart from '@/components/Chart.vue';
-
-
+import Chart from "@/components/Chart.vue";
 @Component({
-  components: { Tabs,Chart },
+  components: { Tabs, Chart }
 })
 export default class Statistics extends Vue {
   tagString(tags: Tag[]) {
     return tags.length === 0 ? "无" : tags.map(t => t.name).join("，");
+  }
+  mounted() {
+    const div = (this.$refs.chartWrapper as HTMLDivElement);
+    div.scrollLeft = div.scrollWidth;
   }
   beautify(string: string) {
     const day = dayjs(string);
@@ -54,32 +57,101 @@ export default class Statistics extends Vue {
       return day.format("YYYY年M月D日");
     }
   }
-    get x() {
-      return {
-        xAxis: {
-          type: 'category',
+  get x() {
+    return {
+      grid: {
+        left: 0,
+        right: 0
+      },
+      xAxis: {
+        type: "category",
+        data: [
+          "1",
+          "2",
+          "3",
+          "4",
+          "5",
+          "6",
+          "7",
+          "8",
+          "9",
+          "10",
+          "11",
+          "12",
+          "13",
+          "14",
+          "15",
+          "16",
+          "17",
+          "18",
+          "19",
+          "20",
+          "21",
+          "22",
+          "23",
+          "24",
+          "25",
+          "26",
+          "27",
+          "28",
+          "29",
+          "30"
+        ],
+        axisTick: { alignWithLabel: true },
+        axisLine: { lineStyle: { color: "#666" } }
+      },
+      yAxis: {
+        type: "value",
+        show: false
+      },
+      series: [
+        {
+          symbol: "circle",
+          symbolSize: 12,
+          itemStyle: { borderWidth: 1, color: "rgb(16,233,248)" },
           data: [
-            '1', '2', '3', '4', '5', '6', '7', '8', '9', '10',
-            '11', '12', '13', '14', '15', '16', '17', '18', '19', '20',
-            '21', '22', '23', '24', '25', '26', '27', '28', '29', '30',
-          ]
-        },
-        yAxis: {
-          type: 'value'
-        },
-        series: [{
-          data: [
-            820, 932, 901, 934, 1290, 1330, 1320,
-            820, 932, 901, 934, 1290, 1330, 1320,
-            820, 932, 901, 934, 1290, 1330, 1320,
-            820, 932, 901, 934, 1290, 1330, 1320, 1, 2
+            820,
+            932,
+            901,
+            934,
+            1290,
+            1330,
+            1320,
+            820,
+            932,
+            901,
+            934,
+            1290,
+            1330,
+            1320,
+            820,
+            932,
+            901,
+            934,
+            1290,
+            1330,
+            1320,
+            820,
+            932,
+            901,
+            934,
+            1290,
+            1330,
+            1320,
+            1,
+            2
           ],
-          type: 'line'
-        }],
-        tooltip: {show: true}
-      };
-    }
-
+          type: "line"
+        }
+      ],
+      tooltip: {
+        show: true,
+        triggerOn: "click",
+        position: "top",
+        formatter: "{c}"
+      }
+    };
+  }
   get recordList() {
     return (this.$store.state as RootState).recordList;
   }
@@ -100,11 +172,9 @@ export default class Statistics extends Vue {
         items: [newList[0]]
       }
     ];
-
     for (let i = 1; i < newList.length; i++) {
       const current = newList[i];
       const last = result[result.length - 1];
-
       if (dayjs(last.title).isSame(dayjs(current.createdAt), "day")) {
         last.items.push(current);
       } else {
@@ -116,6 +186,8 @@ export default class Statistics extends Vue {
     }
     result.map(group => {
       group.total = group.items.reduce((sum, item) => {
+        console.log(sum);
+        console.log(item);
         return sum + item.amount;
       }, 0);
     });
@@ -130,19 +202,19 @@ export default class Statistics extends Vue {
 </script>
 
 <style scoped lang="scss">
-.echarts{
+.echarts {
   max-width: 100%;
   height: 400px;
 }
-.noResult{
- padding: 16px;;
- text-align: center;
+.noResult {
+  padding: 16px;
+  text-align: center;
 }
 ::v-deep {
   .type-tabs-item {
-    background: white;
+    background: #c4c4c4;
     &.selected {
-      background: rgb(114,224,106);
+      background: white;
       &::after {
         display: none;
       }
@@ -170,5 +242,14 @@ export default class Statistics extends Vue {
   margin-right: auto;
   margin-left: 16px;
   color: #999;
+}
+.chart {
+  width: 430%;
+  &-wrapper {
+    overflow: auto;
+    &::-webkit-scrollbar {
+      display: none;
+    }
+  }
 }
 </style>
