beb4a22e6919df023123745bd996dfca2272fe6b
lixiaohu123147 <7595644+lixiaohu123147@user.noreply.gitee.com>
2020-07-21 22:53:08 +0800

    deploy

---
+++ precache-manifest.4e374fba47ac93922baf12929f1ed42b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cd08bf43025bcd89ddd",
+    "url": "/speney-website-1/css/app.71a18e2a.css"
+  },
+  {
+    "revision": "15e470b0467d5698d5ff12ec405477e6",
+    "url": "/speney-website-1/index.html"
+  },
+  {
+    "revision": "5cd08bf43025bcd89ddd",
+    "url": "/speney-website-1/js/app.a05569cd.js"
+  },
+  {
+    "revision": "0aa0723032be82ff5c35",
+    "url": "/speney-website-1/js/chunk-vendors.1d12bcd7.js"
+  },
+  {
+    "revision": "1cd5ad1e24dad22583a9bc457d37fda6",
+    "url": "/speney-website-1/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/speney-website-1/robots.txt"
+  }
+]);
