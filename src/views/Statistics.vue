31c8a8c001a85ccd283ce991b5d88a2497e3d9e4
lixiaohu123147 <7595644+lixiaohu123147@user.noreply.gitee.com>
2020-07-21 15:22:47 +0800

    自我封装一个chart

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,6 +1,7 @@
 <template>
   <Layout>
     <Tabs class-prefix="type" :data-source="recordTypeList" :value.sync="type" />
+    <Chart :options="x"/>
     <ol v-if="groupedList.length>0">
       <li v-for="(group, index) in groupedList" :key="index">
         <h3 class="title">
@@ -27,9 +28,11 @@ import Tabs from "@/components/Tabs.vue";
 import recordTypeList from "@/constants/recordTypeList";
 import dayjs from "dayjs";
 import clone from "@/lib/clone";
+import Chart from '@/components/Chart.vue';
+
 
 @Component({
-  components: { Tabs }
+  components: { Tabs,Chart },
 })
 export default class Statistics extends Vue {
   tagString(tags: Tag[]) {
@@ -51,6 +54,31 @@ export default class Statistics extends Vue {
       return day.format("YYYY年M月D日");
     }
   }
+    get x() {
+      return {
+        xAxis: {
+          type: 'category',
+          data: [
+            '1', '2', '3', '4', '5', '6', '7', '8', '9', '10',
+            '11', '12', '13', '14', '15', '16', '17', '18', '19', '20',
+            '21', '22', '23', '24', '25', '26', '27', '28', '29', '30',
+          ]
+        },
+        yAxis: {
+          type: 'value'
+        },
+        series: [{
+          data: [
+            820, 932, 901, 934, 1290, 1330, 1320,
+            820, 932, 901, 934, 1290, 1330, 1320,
+            820, 932, 901, 934, 1290, 1330, 1320,
+            820, 932, 901, 934, 1290, 1330, 1320, 1, 2
+          ],
+          type: 'line'
+        }],
+        tooltip: {show: true}
+      };
+    }
 
   get recordList() {
     return (this.$store.state as RootState).recordList;
@@ -102,6 +130,10 @@ export default class Statistics extends Vue {
 </script>
 
 <style scoped lang="scss">
+.echarts{
+  max-width: 100%;
+  height: 400px;
+}
 .noResult{
  padding: 16px;;
  text-align: center;
